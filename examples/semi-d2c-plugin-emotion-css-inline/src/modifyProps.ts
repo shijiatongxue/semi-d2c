4549883ae8cbf9b68a3f0c04e3a3b56c19792957
shijia.me <shijia.me@bytedance.com>
2024-04-26 17:58:39 +0800

    fix: [semi-d2c-plugin-emotion-css-inline] fixed empty css`` className bug

--- examples/semi-d2c-plugin-emotion-css-inline/src/modifyProps.ts
+++ examples/semi-d2c-plugin-emotion-css-inline/src/modifyProps.ts
@@ -13,6 +13,11 @@ export default function modifyProps(args) {
       const styleStr = transpileNodeStyleToCSS(node)
         .replace(/[\n]/g, ' ')
         .replace(/  /g, '');
+      // 避免出现 style 有值，但转为 styleStr 后没有值问题，比如有些是默认值会去掉
+      if (styleStr?.trim().length === 0) {
+        delete node.props.className;
+        return;
+      }
       const className = `css\`${styleStr.trim()}\``;
       const classNameVariable = createVariable(className);
       const newProps = { ...props, className: classNameVariable };
